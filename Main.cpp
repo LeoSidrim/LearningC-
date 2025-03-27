c00a3be09f1815f9c1b36c9683788514ebcb1599
Leonardo Sidrim <leonardosidrim2005@gmail.com>
2025-03-27 15:42:35 -0300

    primeira quest att maze

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,42 @@
-/*
-
- UFRGS - Instituto de informática - Curso de C++ - Marcelo Johann - 2004
- 
- Caracteres e Cast:
- - C++ prove conversões automáticas entre os tipos de dados básicos
- - Cast especifica uma conversão explícita, precedendo um valor:
-   a sintaxe velha de C é a segunite: (tipo) 
- Exercícios:
- - experimente inicializar b com 200 em vez de 80
- - declare outros tipos de dados, passe valores de um para outro, 
-   e verifique quais seus valores numéricos imprimindo com cast (int)
- - tente usar cout << int(a) << endl; // e veja que também funciona
-   Neste caso está sendo construído um inteiro com o valor de a.
- */
-
  #include <iostream>
+ #include <stdio.h>
+ #include <string.h>
+
  using namespace std;
  
  int main()
    {
-   char a('X'); 
-   char b = 80; 
+   char cargo[50];
+   float salary;
+   cout << "Qual o seu cargo ?" << endl;
+   cin >> cargo;
+   cout << "Qual seu salario? " << endl;
+   cin >> salary; 
+   int salaryO = salary;
    
-   cout << "a=" << a << endl;
-   cout << "b=" << b << endl;
-   cout << "a=" << (int) a << endl;
-   cout << "b=" << (int) b << endl;  
+   if (strcmp (cargo,"gerente") == 0){
+    cout << "Seu salario antigo: " << salary << endl;
+    salary += (salary*0.1);
+    int salaryN = salary;
+    cout << "Seu novo salário e: " << salary << endl;
+    float dif = salaryN - salaryO;
+    cout << "Diferenca: " << dif << endl;
+
+   } else if (strcmp(cargo,"engenheiro") == 0){
+    cout << "Seu salario antigo: " << salary << endl;
+    salary += (salary*0.2);
+    int salaryN = salary;
+    cout << "Seu novo salário e: " << salary << endl;
+    float dif = salaryN - salaryO;
+    cout << "Diferenca: " << dif << endl;
+   }else{
+    cout << "Seu salario antigo: " << salary << endl;
+    salary += (salary*0.4);
+    int salaryN = salary;
+    cout << "Seu novo salário e: " << salary << endl;
+    float dif = salaryN - salaryO;
+    cout << "Diferenca: " << dif << endl;
+    
+   }
+
    }  
